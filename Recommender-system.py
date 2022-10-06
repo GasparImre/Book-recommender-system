6c5429413dde364490c11068f2478b24373cc5bd
Imre Gáspár <ifj.gaspar.imre@gmail.com>
2022-10-06 19:57:04 +0200

    Added comments, started to make the personalized recommendations
    Put english and hungarian comment into the code just in case. Started to make the personalized part of the recommender system, Already done the dictionary filled with my ratings.

--- Recommender-system.py
+++ Recommender-system.py
@@ -1,10 +1,11 @@
 import pandas as pd
+import  csv
 
 #_______________________KONSTANSOK_____________________________
 ROWS_TO_SHOW = 10 # Sorok számát határozza meg
 MINIMUM_TO_INCLUDE = 20 # A minimum értékelések számát határozza meg
 
-books = pd.read_csv('books-modded.csv',sep=';',encoding='latin')
+books = pd.read_csv('books-modded.csv',sep=';',encoding='latin')                            # Every neccesary csv will be imported there that means 2
 columns_need = ['ISBN','Book-Title','Book-Author','Year-Of-Publication','Publisher']        # Minden szükséges adatot betöltünk ez 3 betöltést jelent
 print(books[columns_need].head(ROWS_TO_SHOW))
 
@@ -16,13 +17,9 @@ users = pd.read_csv('users.csv',sep=';',encoding='latin')
 columns_need = ['User-ID',"Location","Age"]
 print(users[columns_need].head(ROWS_TO_SHOW))
 
-books = books.iloc[:,:-6]
-
-# books =load_data(books,'books-modded.csv',5,['ISBN','Book-Title','Book-Author','Year-Of-Publication','Publisher'])
-# load_data(ratings,'ratings.csv',5,['User-ID',"ISBN","Book-Rating"])
-# load_data(users,'users.csv',5,['User-ID',"Location","Age"])
-
-print(f'____________________________Adatok Betöltve________________________________\n')
+books = books.iloc[:,:-6]       # there is couple of empty columns and just drop those ones there
+                                # a fájl végén van néhány üres oszlop, ezeket dobjuk ki
+print(f'\n____________________________Adatok Betöltve________________________________\n')
 
 joined_datas = ratings.merge(books,how='inner')
 # joined_datas = pd.merge(joined_datas,users,how='inner')
@@ -40,9 +37,9 @@ average_ratings['ratings-count']=joined_datas.groupby('ISBN')['ISBN'].transform(
 average_ratings = average_ratings.loc[average_ratings['ratings-count']>MINIMUM_TO_INCLUDE]
 average_ratings = average_ratings.merge(books, on='ISBN', how='inner')
 
-def recommended_for_a_fan(author,average_ratings):
-    average_ratings = average_ratings.loc[average_ratings['Book-Author'].str.contains(author, na=False)]
-    sorted_average = average_ratings.sort_values(by="Book-Rating", ascending=False)
+def recommended_for_a_fan(author,average_ratings):  # just write the name of the author and the average_ratings dataset, and you got the rated books from that author
+    average_ratings = average_ratings.loc[average_ratings['Book-Author'].str.contains(author, na=False)]    # Az író nevét és a average_ratings-et kell a paraméterekhez írni és már
+    sorted_average = average_ratings.sort_values(by="Book-Rating", ascending=False)                         # ki is írja az értékelt könyveket attól az írótól
     columns_need = ['Book-Rating', 'Book-Title' ]
     # filtered_ratings=sorted_average.where(filter)
     print(f"Egy {author} fannak ajánlott\n{sorted_average[columns_need].head(ROWS_TO_SHOW)}")
@@ -51,7 +48,12 @@ def recommended_for_a_fan(author,average_ratings):
 recommended_for_a_fan("Stephen King",average_ratings)
 recommended_for_a_fan("J. K. Rowling",average_ratings)
 
+#_____________Make the personalized recommendations/Elkészítjük a személyreszabott ajánlásokat__________#
 
+my_rating_dict = {}
+with open("my_ratings.csv",'r') as data:
+    for line in csv.DictReader(data):
+        my_rating_dict.update({str(line['ISBN']): float(line['Rating'])})
 
-
-
+print("Az értékelő dictionary összegyűjtve!")
+print(f"Az értékelésem a 553210424 ISBN számú Metamorphosis-ra {str(my_rating_dict['553210424'])}")
